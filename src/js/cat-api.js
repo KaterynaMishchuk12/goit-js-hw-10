01488f6ab8210962600ca557f0f9a2be25bb892b
KaterynaMishchuk12 <138882300+KaterynaMishchuk12@users.noreply.github.com>
2023-11-30 20:13:49 +0200

    half of task done

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,13 +1,28 @@
-import axios from 'axios';
-axios.defaults.headers.common["x-api-key"] =
-'live_t09FH5qR0cdEhJIKXuBNBkwLbp1vOYBLwbAt1eDxLx8Wvxh7wyKArRccEGVZgvSm';
+// import axios from 'axios';
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_t09FH5qR0cdEhJIKXuBNBkwLbp1vOYBLwbAt1eDxLx8Wvxh7wyKArRccEGVZgvSm';
 
 const BASE_URL = 'https://api.thecatapi.com/v1';
+const api_key =
+  'live_t09FH5qR0cdEhJIKXuBNBkwLbp1vOYBLwbAt1eDxLx8Wvxh7wyKArRccEGVZgvSm';
 
-export function fetchBreeds(catbreed) {
-    return fetch(`${BASE_URL}/breeds`)
-    .then(response => response.json())
+export function fetchBreeds() {
+  return fetch(`${BASE_URL}/breeds?api_key=${api_key}`)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+        console.log(response.data)
+    })    
 };
 
-export function fetchCatByBreed(breedId){}
-
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${BASE_URL}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
+    .then(response => {
+        if(!response.ok)
+        {throw new Error(response.status)}
+        return response.json();
+});
+}
